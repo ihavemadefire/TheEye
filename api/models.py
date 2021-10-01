83763a0dc17e2a58c3c33c52a7643f71f8ed23c2
Jacob <jacobmeide@gmail.com>
2021-10-01 09:59:35 -0500

    add validation to datetime model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from datetime import date
 """Models Module"""
 
 # Create your models here.
@@ -9,6 +11,7 @@ class Event(models.Model):
     category = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
     data = models.JSONField()
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(validators=[MaxValueValidator(limit_value=date.today)])
+    
     def __str__(self):
         return self.session_id + '/' + self.category + '/' + self.name
